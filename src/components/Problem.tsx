6ee1342aa9403bd442076e9f213169c4f1b9ef3a
Nazim Boudeffa <boudeffa@gmail.com>
2024-01-10 15:21:48 +0100

    Added some radial

--- src/components/Problem.tsx
+++ src/components/Problem.tsx
@@ -1,8 +1,8 @@
 export default function Problem() {
     return (
-        <section className="bg-gray-50">
+        <section>
         <div className="p-8 md:p-12 lg:px-16 lg:py-24">
-            <div className="mx-auto max-w-lg text-center">
+            <div className="mx-auto max-w-lg max-h-lg text-center">
             <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
                 We try to solve many digital problems
             </h2>
@@ -12,6 +12,32 @@ export default function Problem() {
             </p>
             </div>
         </div>
+        <svg
+            viewBox="0 0 1024 1024"
+            className="absolute left-1 top-0 -z-10 h-[64rem] w-[64rem] [mask-image:radial-gradient(closest-side,white,transparent)]"
+            aria-hidden="true"
+        >
+            <circle cx={512} cy={512} r={512} fill="url(#top)" fillOpacity="0.7" />
+            <defs>
+            <radialGradient id="top">
+                <stop stopColor="#00FFEE" />
+                <stop offset={1} stopColor="#00FFEE" />
+            </radialGradient>
+            </defs>
+        </svg>
+        <svg
+            viewBox="0 0 1024 1024"
+            className="absolute left-1/2 top-3/4 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+            aria-hidden="true"
+        >
+            <circle cx={512} cy={512} r={512} fill="url(#down)" fillOpacity="0.7" />
+            <defs>
+            <radialGradient id="down">
+                <stop stopColor="#00FF00" />
+                <stop offset={1} stopColor="#00FF00" />
+            </radialGradient>
+            </defs>
+        </svg>
         </section>
     )
 }
